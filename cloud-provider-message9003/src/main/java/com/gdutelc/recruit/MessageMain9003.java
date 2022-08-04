296413a5d95fbce8614ea11ddb44163a8414485c
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-05 00:20:46 +0800

    feat(): 创建SpringBoot启动类

---
+++ cloud-provider-message9003/src/main/java/com/gdutelc/recruit/MessageMain9003.java
@@ -0,0 +1,13 @@
+package com.gdutelc.recruit;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author gregPerlinLi
+ * @date 2022-08-05
+ */
+public class MessageMain9003 {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageMain9003.class, args);
+    }
+}
