eddd7b5954c989681ebcdcaf587eec0f20c2e284
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-05 00:46:24 +0800

    fix(): 添加`@SpringBootApplication`和`@EnableDiscoveryClient`注解

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/RecruitMain91.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/RecruitMain91.java
@@ -1,11 +1,15 @@
 package com.gdutelc.recruit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author gregPerlinLi
  * @date 2022-08-04
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class RecruitMain91 {
     public static void main(String[] args) {
         SpringApplication.run(RecruitMain91.class, args);
--- cloud-provider-message9003/src/main/java/com/gdutelc/recruit/MessageMain9003.java
+++ cloud-provider-message9003/src/main/java/com/gdutelc/recruit/MessageMain9003.java
@@ -1,11 +1,15 @@
 package com.gdutelc.recruit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author gregPerlinLi
  * @date 2022-08-05
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class MessageMain9003 {
     public static void main(String[] args) {
         SpringApplication.run(MessageMain9003.class, args);
