592ac96f3a2dbfd2fdf0117e753c11f93a6a244f
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-05 21:35:15 +0800

    feat( CollegeCode ): 新增学院编号类

--- cloud-api-commons/src/main/java/com/gdutelc/recruit/utils/CollegeCode.java
+++ cloud-api-commons/src/main/java/com/gdutelc/recruit/utils/CollegeCode.java
@@ -10,52 +10,52 @@ public interface CollegeCode {
     /**
      * 机电工程学院
      */
-    public static final int COLLEGE_OF_MECHANICAL_AND_ELECTRICAL_ENGINEERING = 1;
+    int COLLEGE_OF_MECHANICAL_AND_ELECTRICAL_ENGINEERING = 1;
 
     /**
      * 自动化学院
      */
-    public static final int COLLEGE_OF_AUTOMATION = 2;
+    int COLLEGE_OF_AUTOMATION = 2;
 
     /**
      * 轻工化工学院
      */
-    public static final int COLLEGE_OF_LIGHT_INDUSTRY_AND_CHEMICAL_ENGINEERING = 3;
+    int COLLEGE_OF_LIGHT_INDUSTRY_AND_CHEMICAL_ENGINEERING = 3;
 
     /**
      * 信息工程学院
      */
-    public static final int COLLEGE_OF_INFORMATION_ENGINEERING = 4;
+    int COLLEGE_OF_INFORMATION_ENGINEERING = 4;
 
     /**
      * 土木交通工程学院
      */
-    public static final int COLLEGE_OF_CIVIL_AND_TRAFFIC_ENGINEERING = 5;
+    int COLLEGE_OF_CIVIL_AND_TRAFFIC_ENGINEERING = 5;
 
     /**
      * 管理学院
      */
-    public static final int COLLEGE_OF_MANAGEMENT = 6;
+    int COLLEGE_OF_MANAGEMENT = 6;
 
     /**
      * 计算机学院
      */
-    public static final int COLLEGE_OF_COMPUTING = 7;
+    int COLLEGE_OF_COMPUTING = 7;
 
     /**
      * 材料与能源学院
      */
-    public static final int COLLEGE_OF_MATERIALS_AND_ENERGY = 8;
+    int COLLEGE_OF_MATERIALS_AND_ENERGY = 8;
 
     /**
      * 环境科学与工程学院
      */
-    public static final int COLLEGE_OF_ENVIRONMENTAL_SCIENCE_AND_ENGINEERING = 9;
+    int COLLEGE_OF_ENVIRONMENTAL_SCIENCE_AND_ENGINEERING = 9;
 
     /**
      * 外国语学院
      */
-    public static final int COLLEGE_OF_FOREIGN_LANGUAGES = 10;
+    int COLLEGE_OF_FOREIGN_LANGUAGES = 10;
 
     /**
      * 数学与统计学院
@@ -70,51 +70,51 @@ public interface CollegeCode {
     /**
      * 艺术与设计学院
      */
-    public static final int COLLEGE_OF_ART_AND_DESIGN = 13;
+    int COLLEGE_OF_ART_AND_DESIGN = 13;
 
     /**
      * 法学院
      */
-    public static final int COLLEGE_OF_LAW = 14;
+    int COLLEGE_OF_LAW = 14;
 
     /**
      * 建筑与城市规划学院
      */
-    public static final int COLLEGE_OF_ARCHITECTURE_AND_URBAN_PLANNING = 15;
+    int COLLEGE_OF_ARCHITECTURE_AND_URBAN_PLANNING = 15;
 
     /**
      * 经济与贸易学院
      */
-    public static final int COLLEGE_OF_ECONOMICS_AND_TRADE = 16;
+    int COLLEGE_OF_ECONOMICS_AND_TRADE = 16;
 
     /**
      * 生物医药学院
      */
-    public static final int COLLEGE_OF_BIOMEDICINE = 17;
+    int COLLEGE_OF_BIOMEDICINE = 17;
 
     /**
      * 先进制造学院
      */
-    public static final int COLLEGE_OF_ADVANCED_MANUFACTURING = 18;
+    int COLLEGE_OF_ADVANCED_MANUFACTURING = 18;
 
     /**
      * 生态环境与资源学院
      */
-    public static final int COLLEGE_OF_ECOLOGICAL_ENVIRONMENT_AND_RESOURCES = 19;
+    int COLLEGE_OF_ECOLOGICAL_ENVIRONMENT_AND_RESOURCES = 19;
 
     /**
      * 集成电路学院
      */
-    public static final int COLLEGE_OF_INTEGRATED_CIRCUITS = 20;
+    int COLLEGE_OF_INTEGRATED_CIRCUITS = 20;
 
     /**
      * 商学院
      */
-    public static final int COLLEGE_OF_BUSINESS = 21;
+    int COLLEGE_OF_BUSINESS = 21;
 
     /**
      * 国际教育学院
      */
-    public static final int COLLEGE_OF_INTERNATIONAL_EDUCATION = 22;
+    int COLLEGE_OF_INTERNATIONAL_EDUCATION = 22;
 
 }
