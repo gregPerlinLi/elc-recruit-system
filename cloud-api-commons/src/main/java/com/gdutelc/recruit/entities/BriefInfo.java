f544cefefaf8fed28b7408203de9de26c16aecb1
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-05 21:28:16 +0800

    feat( BriefInfo ): 新增面试状态参数 fix( BriefInfo ): 修改注释

--- cloud-api-commons/src/main/java/com/gdutelc/recruit/entities/BriefInfo.java
+++ cloud-api-commons/src/main/java/com/gdutelc/recruit/entities/BriefInfo.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 
 /**
  * 学生简要信息实体类<br/>
- * 简要信息包含学号、姓名、性别、第一意向部门、学院
+ * 简要信息包含学号、姓名、性别、第一意向部门、学院、当前面试状态
  *
  * @author aaa
  * @date 2022-8-5
@@ -17,25 +17,46 @@ import lombok.Setter;
 @AllArgsConstructor
 public class BriefInfo {
 
+    /**
+     * 学号
+     */
     @Getter
     @Setter
     private String stuId;
 
+    /**
+     * 姓名
+     */
     @Getter
     @Setter
     private String name;
 
+    /**
+     * 性别
+     */
     @Getter
     @Setter
     private Integer gender;
 
+    /**
+     * 第一志愿部门
+     */
     @Getter
     @Setter
     private Integer firstDept;
 
+    /**
+     * 学院
+     */
     @Getter
     @Setter
     private Integer college;
 
+    /**
+     * 当前面试状态
+     */
+    @Getter
+    @Setter
+    private Integer status;
 
 }
