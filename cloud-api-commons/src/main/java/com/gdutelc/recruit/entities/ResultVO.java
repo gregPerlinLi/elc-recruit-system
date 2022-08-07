6a63ce5800dfc8f629fb8cf2e371c85f0be879b8
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-07 22:12:16 +0800

    feat( ResultVO ):添加Swagger的API文档说明

--- cloud-api-commons/src/main/java/com/gdutelc/recruit/entities/ResultVO.java
+++ cloud-api-commons/src/main/java/com/gdutelc/recruit/entities/ResultVO.java
@@ -1,5 +1,7 @@
 package com.gdutelc.recruit.entities;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -14,24 +16,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("通用返回消息体")
 public class ResultVO<T> {
 
     /**
      * 消息代码
      */
     @Getter
+    @ApiModelProperty(value = "消息代码")
     private Integer code;
 
     /**
      * 返回信息
      */
     @Getter
+    @ApiModelProperty(value = "返回信息")
     private String msg;
 
     /**
      * 返回前端需要获取的实体对象
      */
     @Getter
+    @ApiModelProperty(value = "返回实体对象")
     private T data;
 
     /**
