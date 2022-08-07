2e37962ba3e4bc5159b1fd0c935ccb1a982afd22
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-07 22:33:38 +0800

    feat( DeptCode ):添加电协部门代号

---
+++ cloud-api-commons/src/main/java/com/gdutelc/recruit/utils/DeptCode.java
@@ -0,0 +1,40 @@
+package com.gdutelc.recruit.utils;
+
+/**
+ * 部门代号
+ *
+ * @author gregPerlinLi
+ * @date 2022-08-07
+ */
+public interface DeptCode {
+    /**
+     * 维修部
+     */
+    int MAINTENANCE_DEPT = 1;
+
+    /**
+     * 秘书部
+     */
+    int SECRETARIAT_DEPT = 2;
+
+    /**
+     * 项目部
+     */
+    int PROJECT_DEPT = 3;
+
+    /**
+     * 实践部
+     */
+    int PRACTICE_DEPT = 4;
+
+    /**
+     * 外联部
+     */
+    int EX_RELATIONS_DEPT = 5;
+
+    /**
+     * 网宣部
+     */
+    int ONLINE_PUBLICITY_DEPT = 6;
+
+}
