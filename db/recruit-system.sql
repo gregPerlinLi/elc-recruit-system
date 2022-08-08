db25a2b924e864ce5c9da54469227f2e4c7bbec2
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-08 11:07:55 +0800

    fix( recruit-system.sql ):将表`poccess`改为`process`

--- db/recruit-system.sql
+++ db/recruit-system.sql
@@ -94,10 +94,10 @@ CREATE TABLE `interviewer_list` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `poccess`
+-- Table structure for table `process`
 --
 
-DROP TABLE IF EXISTS `poccess`;
+DROP TABLE IF EXISTS `process`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `poccess` (
