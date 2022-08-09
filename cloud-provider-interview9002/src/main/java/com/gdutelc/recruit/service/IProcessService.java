49ae73aa11ee7a42ae8b36f297f06a291bc5bec3
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-09 20:55:47 +0800

    feat():编写cloud-provider-interview模块的Service层

---
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/IProcessService.java
@@ -0,0 +1,16 @@
+package com.gdutelc.recruit.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.gdutelc.recruit.domain.entities.Process;
+
+/**
+ * <p>
+ * 面试整体进度 服务类
+ * </p>
+ *
+ * @author gregPerlinLi
+ * @since 2022-08-08
+ */
+public interface IProcessService extends IService<Process> {
+
+}
