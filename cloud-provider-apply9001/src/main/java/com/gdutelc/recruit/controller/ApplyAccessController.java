f9c93cee5c27e3abbff540722f5d2314952536b7
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-09 23:22:13 +0800

    fix( ApplyAccessController ): 请求资源路径头部增加`/pro`以方便区别生产者和消费者

--- cloud-provider-apply9001/src/main/java/com/gdutelc/recruit/controller/ApplyAccessController.java
+++ cloud-provider-apply9001/src/main/java/com/gdutelc/recruit/controller/ApplyAccessController.java
@@ -11,16 +11,15 @@ import org.springframework.web.bind.annotation.*;
  * @date 2022-08-07
  */
 @Controller
-@RequestMapping("/recruit/elc_access")
+@RequestMapping(value = "/pro/recruit/elc_access")
 public class ApplyAccessController {
-
 
     /**
      * 报名接口，提交个人详细信息
      * @param applyInfoDTO 报名信息实体类
-     * @return ResultVO，其中不包含数据，只包含状态码和信息
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
-    @PostMapping("/apply")
+    @PostMapping(value = "/apply")
     @ResponseBody
     public ResultVO apply(ApplyInfoDTO applyInfoDTO){
         return null;
@@ -28,28 +27,35 @@ public class ApplyAccessController {
 
     /**
      * 获取个人完整报名信息接口
-     * @param openid
+     * @param openid 微信openid
      * @return ResultVO，其中数据为个人报名信息实体类
      */
-    @GetMapping("/get_apply_info")
+    @GetMapping(value = "/get_apply_info")
     @ResponseBody
     public ResultVO<ApplyInfoDTO> getApplyInfo(String openid){
         return null;
     }
 
     /**
+     * 获取当前学生的面试状态接口
      *
-     * @param openid
-     * @return
+     * @param openid 微信 openid
+     * @return 面试状态号
      */
-    @GetMapping("/get_status")
+    @GetMapping(value = "/get_status")
     @ResponseBody
     public ResultVO<Integer> getStatus(String openid){
         return null;
     }
 
-
-    @PutMapping("/update_apply_info")
+    /**
+     * 修改学生面试信息接口
+     *
+     * @param openid 微信 openid
+     * @param applyInfoDTO 要更新的学生报名信息
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
+     */
+    @PutMapping(value = "/update_apply_info")
     @ResponseBody
     public ResultVO updateApplyInfo(String openid,ApplyInfoDTO applyInfoDTO){
         return null;
