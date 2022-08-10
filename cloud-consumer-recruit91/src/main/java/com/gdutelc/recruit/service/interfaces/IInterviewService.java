c41927c7b86251e74c3bd73383b21a6baa9800d1
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-10 21:34:56 +0800

    feat( IInterviewService ): 添加示例服务调用

---
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/service/interfaces/IInterviewService.java
@@ -0,0 +1,27 @@
+package com.gdutelc.recruit.service.interfaces;
+
+import com.gdutelc.recruit.domain.vo.ResultVO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 面试模块消费者服务层接口
+ *
+ * @author gregPerlinLi
+ * @date 2022-08-10
+ */
+@Service
+@FeignClient(name = "cloud-provider-interview", contextId = "InterviewServiceApi")
+public interface IInterviewService {
+
+    /**
+     * 示例接口
+     *
+     * @param text 随便一个值，如果为空返回错误
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
+     */
+    @GetMapping(value = "/pro/demo/test/{text}")
+    ResultVO<String> test(@PathVariable("text") String text);
+}
