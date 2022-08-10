7ef21e258bffe1ea0df7b65308edaf403f81083e
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2022-08-10 20:37:49 +0800

    fix(): 修改接口名称

--- cloud-provider-apply9001/src/main/java/com/gdutelc/recruit/service/impl/ApplyServiceImpl.java
+++ cloud-provider-apply9001/src/main/java/com/gdutelc/recruit/service/impl/ApplyServiceImpl.java
@@ -3,7 +3,7 @@ package com.gdutelc.recruit.service.impl;
 import com.gdutelc.recruit.domain.dto.ApplyInfoDTO;
 import com.gdutelc.recruit.domain.vo.ResultVO;
 import com.gdutelc.recruit.mapper.ApplyMapper;
-import com.gdutelc.recruit.service.interfaces.ApplyService;
+import com.gdutelc.recruit.service.interfaces.IApplyService;
 import com.gdutelc.recruit.utils.GenericUtils;
 import com.gdutelc.recruit.utils.ResultStatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +15,7 @@ import javax.annotation.Resource;
  * @author TUFSolareyes
  * @date 22/08/09
  */
-public class ApplyServiceImpl implements ApplyService {
+public class ApplyServiceImpl implements IApplyService {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
