91218de6862b21eb31391472ff187524dfa76f92
gregPerlinLi <lihaolin13@outlook.com>
2022-08-16 21:19:14 +0800

    feat( GlobalExceptionHandler ):新增微服务调用异常处理

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/utils/GlobalExceptionHandler.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/utils/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.gdutelc.recruit.domain.exception.BusinessException;
 import com.gdutelc.recruit.domain.exception.ParamValidateException;
 import com.gdutelc.recruit.domain.exception.ServerException;
 import com.gdutelc.recruit.domain.vo.ResultVO;
+import feign.RetryableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,12 +22,14 @@ public class GlobalExceptionHandler {
     public ResultVO exceptionHandler(Exception e){
         String msg = e.getMessage();
         if ( e instanceof ParamValidateException ) {
-            return new ResultVO(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, msg,null);
+            return new ResultVO(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, msg);
         } else if ( e instanceof BusinessException ) {
-            return new ResultVO(ResultStatusCodeConstant.BUSINESS_EXCEPTION, msg,null);
+            return new ResultVO(ResultStatusCodeConstant.BUSINESS_EXCEPTION, msg);
         } else if ( e instanceof ServerException ) {
-            return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg,null);
+            return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg);
+        } else if (e instanceof RetryableException) {
+            return new ResultVO(ResultStatusCodeConstant.SERVER_TIME_OUT, msg);
         }
-        return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg,null);
+        return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg);
     }
 }
