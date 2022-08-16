75afabd18234d6dc090cc3c192b708b4d6ff88f5
gregPerlinLi <lihaolin13@outlook.com>
2022-08-16 22:57:52 +0800

    feat( GlobalExceptionHandler ):新增登录异常处理

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/utils/GlobalExceptionHandler.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/utils/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gdutelc.recruit.utils;
 
 import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
 import com.gdutelc.recruit.domain.exception.BusinessException;
+import com.gdutelc.recruit.domain.exception.LoginException;
 import com.gdutelc.recruit.domain.exception.ParamValidateException;
 import com.gdutelc.recruit.domain.exception.ServerException;
 import com.gdutelc.recruit.domain.vo.ResultVO;
@@ -21,13 +22,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResultVO exceptionHandler(Exception e){
         String msg = e.getMessage();
+        if ( e instanceof LoginException ) {
+            return new ResultVO(ResultStatusCodeConstant.FORBIDDEN, msg);
+        }
         if ( e instanceof ParamValidateException ) {
             return new ResultVO(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, msg);
-        } else if ( e instanceof BusinessException ) {
+        }
+        if ( e instanceof BusinessException ) {
             return new ResultVO(ResultStatusCodeConstant.BUSINESS_EXCEPTION, msg);
-        } else if ( e instanceof ServerException ) {
+        }
+        if ( e instanceof ServerException ) {
             return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg);
-        } else if (e instanceof RetryableException) {
+        }
+        if (e instanceof RetryableException) {
             return new ResultVO(ResultStatusCodeConstant.SERVER_TIME_OUT, msg);
         }
         return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg);
