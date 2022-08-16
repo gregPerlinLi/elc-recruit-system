746785e0309dfdd396b04807cfbdc926e6df91bc
gregPerlinLi <lihaolin13@outlook.com>
2022-08-16 20:57:03 +0800

    feat( InterviewerListService ):新增生产者面试官登录业务

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/InterviewerListServiceImpl.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/InterviewerListServiceImpl.java
@@ -1,10 +1,17 @@
 package com.gdutelc.recruit.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
 import com.gdutelc.recruit.domain.entities.InterviewerList;
+import com.gdutelc.recruit.domain.vo.ResultVO;
 import com.gdutelc.recruit.mapper.InterviewerListMapper;
 import com.gdutelc.recruit.service.interfaces.IInterviewerListService;
+import com.gdutelc.recruit.utils.GenericUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
 
 /**
  * <p>
@@ -17,4 +24,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class InterviewerListServiceImpl extends ServiceImpl<InterviewerListMapper, InterviewerList> implements IInterviewerListService {
 
+    @Resource
+    InterviewerListMapper interviewerListMapper;
+
+    @Override
+    public ResultVO<Integer> login(String username, String password) {
+        if ( !GenericUtils.allOfNullable(username, password) ) {
+            return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, "参数有误");
+        }
+        QueryWrapper<InterviewerList> queryWrapper = new QueryWrapper<>();
+        HashMap<String, String> params = new HashMap<>(2);
+        params.put("username", username);
+        params.put("password", password);
+        queryWrapper.allEq(params);
+        InterviewerList interviewerList = interviewerListMapper.selectOne(queryWrapper);
+        if ( interviewerList != null ) {
+            return new ResultVO<>(ResultStatusCodeConstant.SUCCESS, "登录成功", interviewerList.getDept());
+        } else {
+            return new ResultVO<>(ResultStatusCodeConstant.FAILED, "用户名或密码错误");
+        }
+    }
 }
--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IInterviewerListService.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IInterviewerListService.java
@@ -2,6 +2,7 @@ package com.gdutelc.recruit.service.interfaces;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.gdutelc.recruit.domain.entities.InterviewerList;
+import com.gdutelc.recruit.domain.vo.ResultVO;
 
 /**
  * <p>
@@ -12,5 +13,14 @@ import com.gdutelc.recruit.domain.entities.InterviewerList;
  * @since 2022-08-08
  */
 public interface IInterviewerListService extends IService<InterviewerList> {
+
+    /**
+     * 面试官登录业务
+     *
+     * @param username 面试官用户名
+     * @param password 密码
+     * @return {@link ResultVO} 其中数据为该面试官所在的部门
+     */
+    ResultVO<Integer> login(String username, String password);
 
 }
