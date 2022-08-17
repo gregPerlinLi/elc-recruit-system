7d54132a7097698b25f0a3718904e656806fcd0d
gregPerlinLi <lihaolin13@outlook.com>
2022-08-17 22:51:32 +0800

    feat( StuInfoService ):新增获取报名者详细信息的方法

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IStuInfoService.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IStuInfoService.java
@@ -1,6 +1,7 @@
 package com.gdutelc.recruit.service.interfaces;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.gdutelc.recruit.domain.dto.DetailedInfoDTO;
 import com.gdutelc.recruit.domain.entities.StuInfo;
 
 /**
@@ -12,5 +13,13 @@ import com.gdutelc.recruit.domain.entities.StuInfo;
  * @since 2022-08-08
  */
 public interface IStuInfoService extends IService<StuInfo> {
+
+    /**
+     * 获取报名者详细信息
+     *
+     * @param stuId 报名者学号
+     * @return {@link DetailedInfoDTO} 报名者详细信息实体类
+     */
+    DetailedInfoDTO detailedApplyQuery(String stuId);
 
 }
