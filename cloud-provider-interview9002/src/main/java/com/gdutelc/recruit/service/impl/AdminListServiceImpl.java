84b931328993253949188644d580c92ed428fd11
gregPerlinLi <lihaolin13@outlook.com>
2022-08-17 20:44:03 +0800

    feat( AdminListService ):添加管理员登录业务

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/AdminListServiceImpl.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/AdminListServiceImpl.java
@@ -1,10 +1,20 @@
 package com.gdutelc.recruit.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gdutelc.recruit.constant.RedisKeyConstant;
+import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
 import com.gdutelc.recruit.domain.entities.AdminList;
+import com.gdutelc.recruit.domain.vo.ResultVO;
 import com.gdutelc.recruit.mapper.AdminListMapper;
 import com.gdutelc.recruit.service.interfaces.IAdminListService;
+import com.gdutelc.recruit.utils.GenericUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -16,5 +26,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AdminListServiceImpl extends ServiceImpl<AdminListMapper, AdminList> implements IAdminListService {
+    @Resource
+    AdminListMapper adminListMapper;
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public ResultVO<String> login(String username, String password, String sessionId) {
+        if ( !GenericUtils.allOfNullable(username, password) ) {
+            return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, "参数有误");
+        }
+        QueryWrapper<AdminList> queryWrapper = new QueryWrapper<>();
+        HashMap<String, String> params = new HashMap<>(2);
+        params.put("username", username);
+        params.put("password", password);
+        queryWrapper.allEq(params);
+        AdminList adminList = adminListMapper.selectOne(queryWrapper);
+        if ( adminList != null ) {
+            stringRedisTemplate.opsForValue().set(RedisKeyConstant.LOGIN_USER + username, sessionId, 30, TimeUnit.MINUTES);
+            return new ResultVO<>(ResultStatusCodeConstant.SUCCESS, "登录成功", adminList.getUsername());
+        } else {
+            return new ResultVO<>(ResultStatusCodeConstant.FAILED, "用户名或密码错误");
+        }
+    }
 }
