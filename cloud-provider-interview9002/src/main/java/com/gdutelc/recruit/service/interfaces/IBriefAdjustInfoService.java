b2c69143e13a3dcfedec17e30734a6a8718b4b64
gregPerlinLi <lihaolin13@outlook.com>
2022-08-18 21:40:16 +0800

    feat( BriefAdjustInfoService ):新增调剂学生简要信息业务接口

---
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IBriefAdjustInfoService.java
@@ -0,0 +1,25 @@
+package com.gdutelc.recruit.service.interfaces;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.gdutelc.recruit.domain.dto.BriefAdjustInfoDTO;
+import com.gdutelc.recruit.domain.dto.BriefInfoDTO;
+import com.gdutelc.recruit.domain.dto.PageDTO;
+
+/**
+ * 学生简要信息 服务类
+ *
+ * @author gregPerlinLi
+ * @date 2022-08-17
+ */
+public interface IBriefAdjustInfoService {
+
+    /**
+     * 获取调剂报名者简要报名信息集合
+     *
+     * @param page 需要查询第几页
+     * @param limit 每一页的列数限制
+     * @return {@link Page} 调剂报名者简要信息分页集合
+     */
+    PageDTO<BriefAdjustInfoDTO> briefAdjustApplyQuery(Integer page, Integer limit);
+
+}
