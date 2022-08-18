8016633fc35774553b0bf3d565ad398e3579871f
gregPerlinLi <lihaolin13@outlook.com>
2022-08-18 22:12:56 +0800

    feat( ICommentService ): 新增查询接口
    新增查询学生所有评价接口

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/ICommentService.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/ICommentService.java
@@ -1,9 +1,13 @@
 package com.gdutelc.recruit.service.interfaces;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.gdutelc.recruit.domain.dto.PageDTO;
 import com.gdutelc.recruit.domain.entities.Comment;
 import com.gdutelc.recruit.domain.vo.ResultVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -23,4 +27,13 @@ public interface ICommentService extends IService<Comment> {
      * @throws IllegalAccessException 非法访问异常
      */
     ResultVO addComment(Comment comment) throws IllegalAccessException;
+
+    /**
+     * 查询学生的所有评价
+     *
+     * @param stuId 需要查询的学生学号
+     * @return {@link ResultVO}，其中数据为该报名者的评价集合
+     * @author gregPerlinLi
+     */
+    ResultVO<List<Comment>> queryComment(String stuId);
 }
