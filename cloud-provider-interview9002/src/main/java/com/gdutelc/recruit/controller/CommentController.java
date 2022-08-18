6159b1dbdcf916f2c2e081dfb5d96424593812d1
gregPerlinLi <lihaolin13@outlook.com>
2022-08-18 22:34:40 +0800

    feat( CommentController ):新增生产者接口
    新增生产者获取报名者所有评价接口

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/controller/CommentController.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/controller/CommentController.java
@@ -6,6 +6,7 @@ import com.gdutelc.recruit.service.interfaces.ICommentService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author TUFSolareyes
@@ -28,5 +29,16 @@ public class CommentController {
     @PostMapping(value = "/publish_comment")
     public ResultVO addComment(@RequestBody Comment comment) throws IllegalAccessException {
         return commentService.addComment(comment);
+    }
+
+    /**
+     * 查询学生的所有评价
+     *
+     * @param stuId 需要查询的学生学号
+     * @return {@link ResultVO}，其中数据为该报名者的评价集合
+     */
+    @PostMapping(value = "/query_comments/{stu_id}")
+    private ResultVO<List<Comment>> queryComments(@PathVariable("stu_id") String stuId) {
+        return commentService.queryComment(stuId);
     }
 }
