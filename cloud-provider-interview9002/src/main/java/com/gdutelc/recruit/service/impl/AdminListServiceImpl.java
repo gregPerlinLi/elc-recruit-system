4dfcdd8e512456d3b0ae267d1dd748bd02659d98
gregPerlinLi <lihaolin13@outlook.com>
2022-08-23 21:15:26 +0800

    feat( InterviewerListService ):新增业务
    新增管理员退出业务

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/AdminListServiceImpl.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/AdminListServiceImpl.java
@@ -50,4 +50,13 @@ public class AdminListServiceImpl extends ServiceImpl<AdminListMapper, AdminList
             return new ResultVO<>(ResultStatusCodeConstant.FAILED, "用户名或密码错误");
         }
     }
+
+    @Override
+    public ResultVO<Void> logout(String username) throws IllegalAccessException {
+        if ( !GenericUtils.allOfNullable(username) ) {
+            return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, "参数有误");
+        }
+        stringRedisTemplate.delete(RedisKeyConstant.loginUserWith(username));
+        return new ResultVO<>(ResultStatusCodeConstant.SUCCESS, "退出成功");
+    }
 }
