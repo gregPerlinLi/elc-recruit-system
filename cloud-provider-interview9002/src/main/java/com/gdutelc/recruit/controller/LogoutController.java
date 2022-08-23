48ceccdb9cb5afa87a2fe0654d931f7d69d093b8
gregPerlinLi <lihaolin13@outlook.com>
2022-08-23 21:22:50 +0800

    feat( LogoutController ):新增生产者接口
    新增生产者面试官和管理员的退出接口

---
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/controller/LogoutController.java
@@ -0,0 +1,52 @@
+package com.gdutelc.recruit.controller;
+
+import com.gdutelc.recruit.domain.vo.ResultVO;
+import com.gdutelc.recruit.service.interfaces.IAdminListService;
+import com.gdutelc.recruit.service.interfaces.IInterviewerListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 面试官和管理员退出接口
+ *
+ * @author gregPerlinLi
+ * @date 2022-08-23
+ */
+@RestController
+@RequestMapping("/pro/interview/elc_access")
+public class LogoutController {
+
+    @Resource
+    IInterviewerListService interviewerListService;
+
+    @Resource
+    IAdminListService adminListService;
+
+    /**
+     * 面试官退出接口
+     *
+     * @param username 退出用户名
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
+     * @throws IllegalAccessException 非法访问异常
+     */
+    @GetMapping(value = "interviewer_logout/{username}")
+    public ResultVO<Void> interviewerLogout(@PathVariable("username") String username) throws IllegalAccessException {
+        return interviewerListService.logout(username);
+    }
+
+    /**
+     * 管理员退出接口
+     *
+     * @param username 退出用户名
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
+     * @throws IllegalAccessException 非法访问异常
+     */
+    @GetMapping(value = "admin_logout/{username}")
+    public ResultVO<Void> adminLogout(@PathVariable("username") String username) throws IllegalAccessException {
+        return adminListService.logout(username);
+    }
+}
