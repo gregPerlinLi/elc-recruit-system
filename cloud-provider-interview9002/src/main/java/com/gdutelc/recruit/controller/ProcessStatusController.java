eef0e114cf70b2e1989c6e61e2d44466d06a47e6
gregPerlinLi <lihaolin13@outlook.com>
2022-08-25 23:19:33 +0800

    fix( ProcessStatusController ):添加请求链接
    添加请求链接

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
@@ -3,6 +3,7 @@ package com.gdutelc.recruit.controller;
 import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
 import com.gdutelc.recruit.domain.vo.ResultVO;
 import com.gdutelc.recruit.service.interfaces.IProcessService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ public class ProcessStatusController {
      *
      * @return {@link ResultVO}，其中数据为当前面试总进度
      */
+    @GetMapping(value = "/get_now_process")
     public ResultVO<Integer> getNowProcess() {
         return new ResultVO<>(ResultStatusCodeConstant.SUCCESS, "获取成功", processService.getNowProcess());
     }
