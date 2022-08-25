ce029f215d408734cedf28fb3bd4d1ca9582ba83
gregPerlinLi <lihaolin13@outlook.com>
2022-08-25 23:27:43 +0800

    feat( ProcessStatusController ):添加API接口类
    添加面试总进度相关API接口类

---
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
@@ -0,0 +1,17 @@
+package com.gdutelc.recruit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 面试总进度相关接口
+ *
+ * @author gregPerlinLi
+ * @date 2022-08-25
+ */
+@RestController
+@RequestMapping("/interview/elc_access/process_status_code")
+public class ProcessStatusController {
+
+
+}
