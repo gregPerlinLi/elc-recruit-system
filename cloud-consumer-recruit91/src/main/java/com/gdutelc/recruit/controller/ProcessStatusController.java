c694529d8e14c62dde3ebb1e11f2a5066702a6b8
gregPerlinLi <lihaolin13@outlook.com>
2022-08-25 23:28:05 +0800

    feat( ProcessStatusController ):添加API接口
    添加获取面试总进度API接口

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
@@ -1,7 +1,11 @@
 package com.gdutelc.recruit.controller;
 
+import com.gdutelc.recruit.domain.vo.ResultVO;
+import com.gdutelc.recruit.service.interfaces.IInterviewService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * 面试总进度相关接口
@@ -12,6 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/interview/elc_access/process_status_code")
 public class ProcessStatusController {
+    @Resource
+    IInterviewService interviewService;
 
+    /**
+     * 获取当前面试总进度
+     *
+     * @return {@link ResultVO}，其中数据为当前面试总进度
+     */
+    @RequestMapping(value = "/get_now_process")
+    public ResultVO<Integer> getNowProcess() {
+        return interviewService.getNowProcess();
+    }
 
 }
