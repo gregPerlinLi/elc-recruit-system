cbe81cffebd1f8a0f72b5d00affe9d218e679afd
gregPerlinLi <lihaolin13@outlook.com>
2022-08-25 23:17:36 +0800

    feat( ProcessStatusController ):新增接口类
    新增获取面试总进度接口类

---
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
@@ -0,0 +1,32 @@
+package com.gdutelc.recruit.controller;
+
+import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
+import com.gdutelc.recruit.domain.vo.ResultVO;
+import com.gdutelc.recruit.service.interfaces.IProcessService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 面试总进度相关接口
+ *
+ * @author gregPerlinLi
+ * @date 2022-08-25
+ */
+@RestController
+@RequestMapping("/pro/interview/elc_access/process_status_code")
+public class ProcessStatusController {
+
+    @Resource
+    IProcessService processService;
+
+    /**
+     * 获取当前面试总进度
+     *
+     * @return {@link ResultVO}，其中数据为当前面试总进度
+     */
+    public ResultVO<Integer> getNowProcess() {
+        return new ResultVO<>(ResultStatusCodeConstant.SUCCESS, "获取成功", processService.getNowProcess());
+    }
+}
