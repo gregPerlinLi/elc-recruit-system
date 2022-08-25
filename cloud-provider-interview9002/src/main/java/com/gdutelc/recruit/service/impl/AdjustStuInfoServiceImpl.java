572e859e05ecec719e14e264a7ad5748ea519a08
gregPerlinLi <lihaolin13@outlook.com>
2022-08-25 20:30:33 +0800

    feat( AdjustStuInfoService ):添加业务
    添加开始面试业务

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/AdjustStuInfoServiceImpl.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/impl/AdjustStuInfoServiceImpl.java
@@ -1,10 +1,16 @@
 package com.gdutelc.recruit.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
+import com.gdutelc.recruit.constant.StudentStatusConstant;
 import com.gdutelc.recruit.domain.entities.AdjustStuInfo;
 import com.gdutelc.recruit.mapper.AdjustStuInfoMapper;
 import com.gdutelc.recruit.service.interfaces.IAdjustStuInfoService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +23,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdjustStuInfoServiceImpl extends ServiceImpl<AdjustStuInfoMapper, AdjustStuInfo> implements IAdjustStuInfoService {
 
+    @Resource
+    AdjustStuInfoMapper adjustStuInfoMapper;
+
+    @Override
+    public Integer interviewStart(String stuId) {
+        QueryWrapper<AdjustStuInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("stu_id", stuId);
+        AdjustStuInfo adjustStuInfo = getOne(queryWrapper);
+        if ( adjustStuInfo == null ) {
+            return 0;
+        } else {
+            UpdateWrapper<AdjustStuInfo> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.eq("stu_id", stuId);
+            updateWrapper.eq("status", StudentStatusConstant.CHECKED_IN);
+            adjustStuInfo.setStatus(StudentStatusConstant.INTERVIEWING);
+            int update = adjustStuInfoMapper.update(adjustStuInfo, updateWrapper);
+            if ( update == 1 ) {
+                return StudentStatusConstant.INTERVIEWING;
+            } else {
+                return ResultStatusCodeConstant.FAILED;
+            }
+        }
+    }
 }
