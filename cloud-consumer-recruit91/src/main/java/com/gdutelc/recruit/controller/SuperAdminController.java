c7d02cd577c72945ab90a46c2446c188d0cba1d2
gregPerlinLi <lihaolin13@outlook.com>
2022-08-26 22:53:46 +0800

    fix( SuperAdminController ):提取方法
    提取所有的Sentinel限流处理方法

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/SuperAdminController.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/SuperAdminController.java
@@ -1,7 +1,10 @@
 package com.gdutelc.recruit.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.gdutelc.recruit.domain.dto.DetailedInfoDTO;
 import com.gdutelc.recruit.domain.entities.StuInfo;
 import com.gdutelc.recruit.domain.vo.ResultVO;
+import com.gdutelc.recruit.utils.SentinelBlockHandler;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +29,7 @@ public class SuperAdminController {
      * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     @PutMapping(value = "/process_advance")
+    @SentinelResource(value = "processAdvance", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "processAdvanceBlockHandler")
     @ApiOperation(value = "面试进度推进", tags = "super_admin", response = ResultVO.class)
     public ResultVO processAdvance() {
         return null;
@@ -37,6 +41,7 @@ public class SuperAdminController {
      * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     @GetMapping(value = "/first_interview_notify")
+    @SentinelResource(value = "firstInterviewNotify", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "firstInterviewNotifyBlockHandler")
     @ApiOperation(value = "发送一面通知", tags = "super_admin", response = ResultVO.class)
     public ResultVO firstInterviewNotify() {
         return null;
@@ -48,6 +53,7 @@ public class SuperAdminController {
      * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     @GetMapping(value = "/second_interview_notify")
+    @SentinelResource(value = "secondInterviewNotify", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "secondInterviewNotifyBlockHandler")
     @ApiOperation(value = "发送二面通知", tags = "super_admin", response = ResultVO.class)
     public ResultVO secondInterviewNotify() {
         return null;
@@ -59,6 +65,7 @@ public class SuperAdminController {
      * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     @GetMapping(value = "/written_test_notify")
+    @SentinelResource(value = "writtenTestNotify", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "writtenTestNotifyBlockHandler")
     @ApiOperation(value = "发送笔试通知", tags = "super_admin", response = ResultVO.class)
     public ResultVO writtenTestNotify() {
         return null;
@@ -70,6 +77,7 @@ public class SuperAdminController {
      * @return {@link ResultVO}，其中包含数据{@link List<StuInfo>}，和状态码和信息
      */
     @GetMapping(value = "/get_first_interview_pass_list")
+    @SentinelResource(value = "getFirstInterviewPassList", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "getFirstInterviewPassListBlockHandler")
     @ApiOperation(value = "获取一面通过的学生列表", tags = "super_admin", response = ResultVO.class)
     public ResultVO<List<StuInfo>> getFirstInterviewPassList() {
         return null;
@@ -81,6 +89,7 @@ public class SuperAdminController {
      * @return {@link ResultVO}，其中包含数据{@link List<StuInfo>}，和状态码和信息
      */
     @GetMapping(value = "/get_final_admission_list")
+    @SentinelResource(value = "getFinalAdmissionList", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "getFinalAdmissionListBlockHandler")
     @ApiOperation(value = "获取电协最终录取的所有面试者名单", tags = "super_admin", response = ResultVO.class)
     public ResultVO<List<StuInfo>> getFinalAdmissionList() {
         return null;
