4e7bc1691cdcf860d27cdb62f4045f9d108c7668
gregPerlinLi <lihaolin13@outlook.com>
2022-08-26 22:51:32 +0800

    fix( LoginController ):提取方法
    提取所有的Sentinel限流处理方法

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/LoginController.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/LoginController.java
@@ -1,12 +1,12 @@
 package com.gdutelc.recruit.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.gdutelc.recruit.domain.vo.ResultVO;
 import com.gdutelc.recruit.domain.wx.LoginInfo;
 import com.gdutelc.recruit.service.interfaces.IInterviewService;
 import com.gdutelc.recruit.service.interfaces.IMessageService;
 import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
+import com.gdutelc.recruit.utils.SentinelBlockHandler;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,7 +41,7 @@ public class LoginController {
      * @return {@link ResultVO}，其中数据为该学生姓名
      */
     @GetMapping(value = "/login/{js_code}/{grant_type}")
-    @SentinelResource(value = "login", blockHandler = "loginHandlerException")
+    @SentinelResource(value = "login", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "loginHandlerException")
     @ApiOperation(value = "登录", tags = "login", response = ResultVO.class)
     public ResultVO<String> login(@ApiParam(value = "微信登录code", required = true) @PathVariable("js_code") String jsCode,
                                   @ApiParam(value = "授权类型", required = true) @PathVariable("grant_type") String grantType) {
@@ -61,7 +61,7 @@ public class LoginController {
      * @return {@link ResultVO}，其中数据为该面试官所在部门
      */
     @GetMapping(value = "/interviewer_login/{username}/{password}")
-    @SentinelResource(value = "interviewerLogin", blockHandler = "interviewerLoginHandlerException")
+    @SentinelResource(value = "interviewerLogin", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "interviewerLoginHandlerException")
     @ApiOperation(value = "面试官登录", tags = "login", response = ResultVO.class)
     public ResultVO<Integer> interviewerLogin(@ApiParam(value = "用户名", required = true) @PathVariable("username") String username,
                                               @ApiParam(value = "密码（加密后的）", required = true) @PathVariable("password") String password,
@@ -82,7 +82,7 @@ public class LoginController {
      * @return {@link ResultVO}，其中数据为该管理员的用户名
      */
     @GetMapping(value = "/admin_login/{username}/{password}")
-    @SentinelResource(value = "adminLogin", blockHandler = "adminLoginHandlerException")
+    @SentinelResource(value = "adminLogin", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "adminLoginHandlerException")
     @ApiOperation(value = "管理员登录", tags = "login", response = ResultVO.class)
     public ResultVO<String> adminLogin(@ApiParam(value = "用户名", required = true) @PathVariable("username") String username,
                                        @ApiParam(value = "密码（加密后的）", required = true) @PathVariable("password") String password,
@@ -92,35 +92,6 @@ public class LoginController {
             request.getSession().setAttribute("username", username);
         }
         return result;
-    }
-
-    /**
-     * Sentinel 异常处理 —— 学生登录接口
-     */
-    public ResultVO<Integer> loginHandlerException(@PathVariable("js_code") String jsCode,
-                                                   @PathVariable("grant_type") String grantType,
-                                                   BlockException exception) {
-        return new ResultVO<>(ResultStatusCodeConstant.TO_MANY_REQUEST, exception.getClass().getCanonicalName() + "\t REQUEST BLOCKED BY SENTINEL ...");
-    }
-
-    /**
-     * Sentinel 异常处理 —— 面试官登录接口
-     */
-    public ResultVO<String> interviewerLoginHandlerException(@PathVariable("username") String username,
-                                                             @PathVariable("password") String password,
-                                                             HttpServletRequest request,
-                                                             BlockException exception) {
-        return new ResultVO<>(ResultStatusCodeConstant.TO_MANY_REQUEST, exception.getClass().getCanonicalName() + "\t REQUEST BLOCKED BY SENTINEL ...");
-    }
-
-    /**
-     * Sentinel 异常处理 —— 管理员登录接口
-     */
-    public ResultVO<String> adminLoginHandlerException(@PathVariable("username") String username,
-                                                       @PathVariable("password") String password,
-                                                       HttpServletRequest request,
-                                                       BlockException exception) {
-        return new ResultVO<>(ResultStatusCodeConstant.TO_MANY_REQUEST, exception.getClass().getCanonicalName() + "\t REQUEST BLOCKED BY SENTINEL ...");
     }
 
 }
