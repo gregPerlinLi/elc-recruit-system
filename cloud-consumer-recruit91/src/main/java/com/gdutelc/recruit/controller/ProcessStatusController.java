4e309d3bda14a89ae8d23c7e22888e7d1cc7869d
gregPerlinLi <lihaolin13@outlook.com>
2022-08-26 22:51:53 +0800

    fix( ProcessStatusController ):提取方法
    提取所有的Sentinel限流处理方法

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/controller/ProcessStatusController.java
@@ -1,7 +1,10 @@
 package com.gdutelc.recruit.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.gdutelc.recruit.domain.vo.ResultVO;
 import com.gdutelc.recruit.service.interfaces.IInterviewService;
+import com.gdutelc.recruit.utils.SentinelBlockHandler;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,8 @@ public class ProcessStatusController {
      * @return {@link ResultVO}，其中数据为当前面试总进度
      */
     @GetMapping(value = "/get_now_process")
+    @SentinelResource(value = "getNowProcess", blockHandlerClass = SentinelBlockHandler.class, blockHandler = "getNowProcessHandlerException")
+    @ApiOperation(value = "获取当前面试进度", tags = "processStatus", response = ResultVO.class)
     public ResultVO<Integer> getNowProcess() {
         return interviewService.getNowProcess();
     }
