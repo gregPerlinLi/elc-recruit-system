ca39b00ccdae40724fb6040857f4bbc39ab40964
gregPerlinLi <lihaolin13@outlook.com>
2022-08-28 20:45:45 +0800

    fix( AdjustStuInfoService ):修改业务
    将二面录取成功的学生写入`admission_list`表中

--- cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IAdjustStuInfoService.java
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/service/interfaces/IAdjustStuInfoService.java
@@ -27,7 +27,7 @@ public interface IAdjustStuInfoService extends IService<AdjustStuInfo> {
      * 开始调剂面试
      *
      * @param stuId 开始面试的学生学号
-     * @return 学生当前状态码（{@code 450}代表学生第一志愿部门和面试官所在部门不一致，{@code 0}代表不存在此学生，{@code 444}代表由于学生状态不符合要求，请求失败）
+     * @return 学生当前状态码（{@code 0}代表不存在此学生，{@code 444}代表由于学生状态不符合要求，请求失败）
      */
     Integer interviewStart(String stuId);
 
@@ -35,7 +35,8 @@ public interface IAdjustStuInfoService extends IService<AdjustStuInfo> {
      * 通过调剂面试
      *
      * @param stuId 通过面试的学生学号
-     * @return 学生当前状态码（{@code 450}代表学生第一志愿部门和面试官所在部门不一致，{@code 0}代表不存在此学生，{@code 444}代表由于学生状态不符合要求，请求失败）
+     * @param interviewerUsername 面试官用户名
+     * @return 学生当前状态码（{@code 450}代表该学生已被录取，{@code 0}代表不存在此学生，{@code 444}代表由于学生状态不符合要求，请求失败）
      */
-    Integer interviewPass(String stuId);
+    Integer interviewPass(String stuId, String interviewerUsername);
 }
