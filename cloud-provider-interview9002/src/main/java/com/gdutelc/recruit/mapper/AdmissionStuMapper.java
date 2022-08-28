99444af5673637aa056beaf389292f98877dfb89
gregPerlinLi <lihaolin13@outlook.com>
2022-08-28 20:40:19 +0800

    feat( AdmissionStuMapper ):新增Mapper接口
    新增最终录取学生列表 Mapper 接口

---
+++ cloud-provider-interview9002/src/main/java/com/gdutelc/recruit/mapper/AdmissionStuMapper.java
@@ -0,0 +1,16 @@
+package com.gdutelc.recruit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gdutelc.recruit.domain.entities.AdmissionStu;
+
+/**
+ * <p>
+ * 录取学生列表 Mapper 接口
+ * </p>
+ *
+ * @author gregPerlinLi
+ * @since 2022-08-28
+ */
+public interface AdmissionStuMapper extends BaseMapper<AdmissionStu> {
+
+}
