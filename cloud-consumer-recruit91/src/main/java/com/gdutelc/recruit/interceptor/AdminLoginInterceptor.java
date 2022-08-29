0a7b6abdafcad04a3da2221a15de3af7efcad15c
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-08-29 21:37:17 +0800

    fix( AdminLoginInterceptor ):修改Session判断机制
    修改Session判断机制，当不存在此Session时直接返回`null`

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/interceptor/AdminLoginInterceptor.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/interceptor/AdminLoginInterceptor.java
@@ -22,7 +22,7 @@ public class AdminLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         IInterviewService interviewService = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getServletContext()).getBean(IInterviewService.class);
-        String username = (String) request.getSession().getAttribute("admin_username");
+        String username = (String) request.getSession(false).getAttribute("admin_username");
         String sessionId = request.getSession().getId();
         if ( username != null ) {
             ResultVO<String> result = interviewService.loginVerify(username, sessionId);
