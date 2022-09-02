65d78f1786b7738993c2cb8e5b9a934027c19c71
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-09-02 20:42:58 +0800

    fix( IMessageService ):修改服务调用
    修改服务调用的返回类型

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/service/interfaces/IMessageService.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/service/interfaces/IMessageService.java
@@ -1,5 +1,6 @@
 package com.gdutelc.recruit.service.interfaces;
 
+import com.gdutelc.recruit.domain.dto.AdmissionStuDTO;
 import com.gdutelc.recruit.domain.entities.BriefPasserInfo;
 import com.gdutelc.recruit.domain.entities.StuInfo;
 import com.gdutelc.recruit.domain.vo.ResultVO;
@@ -43,11 +44,11 @@ public interface IMessageService {
     /**
      * 获取电协最终录取的所有面试者名单
      *
-     * @return {@link ResultVO}，其中包含数据{@link List<StuInfo>}，和状态码和信息
-     * @param dept 社团部门
+     * @param dept 社团部门，{@code 0}代表全选
+     * @return {@link ResultVO}，其中包含数据{@link List<AdmissionStuDTO>}，和状态码和信息
      */
     @GetMapping(value = "/pro/super_admin/elc_access/get_final_admission_list/{dept}")
-    ResultVO<List<BriefPasserInfo>> getFinalAdmissionList(@PathVariable("dept") Integer dept);
+    ResultVO<List<AdmissionStuDTO>> getFinalAdmissionList(@PathVariable("dept") Integer dept);
 
     /**
      * 向用户发送第一次面试提醒
