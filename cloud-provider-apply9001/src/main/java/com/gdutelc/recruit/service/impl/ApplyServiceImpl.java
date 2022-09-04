f7218744743a6abd862dcd333336179f61abc12e
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-09-04 12:10:32 +0800

    fix( ApplyServiceImpl ):添加状态判断
    添加对当前面试总进度状态的判断，一旦进度推进到一面开始则不再允许报名和修改报名信息

--- cloud-provider-apply9001/src/main/java/com/gdutelc/recruit/service/impl/ApplyServiceImpl.java
+++ cloud-provider-apply9001/src/main/java/com/gdutelc/recruit/service/impl/ApplyServiceImpl.java
@@ -2,6 +2,7 @@ package com.gdutelc.recruit.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.gdutelc.recruit.constant.RecruitStatusConstant;
 import com.gdutelc.recruit.constant.RedisKeyConstant;
 import com.gdutelc.recruit.domain.dto.ApplyInfoDTO;
 import com.gdutelc.recruit.domain.vo.ResultVO;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * 招新生产者接口
@@ -32,15 +34,18 @@ public class ApplyServiceImpl implements IApplyService {
     @Override
     public ResultVO<String> apply(ApplyInfoDTO applyInfoDTO) throws IllegalAccessException {
         //判空
-        if(!GenericUtils.allOfNullable(applyInfoDTO)){
+        if ( !GenericUtils.allOfNullable(applyInfoDTO) ) {
             return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION,"参数有误",null);
         }
         String openid = applyInfoDTO.getOpenid();
         //判断openid
-        if(Boolean.FALSE.equals(stringRedisTemplate.opsForSet().isMember(RedisKeyConstant.STU_OPENID, openid))){
+        if ( Boolean.FALSE.equals(stringRedisTemplate.opsForSet().isMember(RedisKeyConstant.STU_OPENID, openid)) ) {
             return new ResultVO<>(ResultStatusCodeConstant.FORBIDDEN,"openid缺失",null);
         }
-
+        // 判断是否已经到了开始面试的进度
+        if ( !Objects.equals(stringRedisTemplate.opsForValue().get(RedisKeyConstant.PROCESS), Integer.toString(RecruitStatusConstant.APPLY))) {
+            return new ResultVO<>(ResultStatusCodeConstant.FORBIDDEN,"不在报名阶段",null);
+        }
         applyMapper.insert(applyInfoDTO);
         stringRedisTemplate.opsForSet().remove(RedisKeyConstant.STU_OPENID, openid);
         return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"报名成功",applyInfoDTO.getName());
@@ -48,13 +53,13 @@ public class ApplyServiceImpl implements IApplyService {
 
     @Override
     public ResultVO<ApplyInfoDTO> getApplyInfo(String openid) {
-        if(!GenericUtils.ofNullable(openid)){
+        if ( !GenericUtils.ofNullable(openid) ) {
             return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION,"参数有误",null);
         }
         QueryWrapper<ApplyInfoDTO> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("openid",openid);
         ApplyInfoDTO applyInfoDTO = applyMapper.selectOne(queryWrapper);
-        if(applyInfoDTO != null){
+        if ( applyInfoDTO != null ) {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"获取信息成功",applyInfoDTO);
         }
         return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND,"搜索无果",null);
@@ -62,13 +67,13 @@ public class ApplyServiceImpl implements IApplyService {
 
     @Override
     public ResultVO<Integer> getStatus(String openid) {
-        if(!GenericUtils.ofNullable(openid)){
+        if ( !GenericUtils.ofNullable(openid) ) {
             return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION,"参数有误",null);
         }
         QueryWrapper<ApplyInfoDTO> queryWrapper = new QueryWrapper<>();
         queryWrapper.select("status").eq("openid",openid);
         ApplyInfoDTO applyInfoDTO = applyMapper.selectOne(queryWrapper);
-        if(applyInfoDTO != null && applyInfoDTO.getStatus() != null){
+        if ( applyInfoDTO != null && applyInfoDTO.getStatus() != null ) {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"获取信息成功",applyInfoDTO.getStatus());
         }
         return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND,"搜索无果",null);
@@ -76,22 +81,26 @@ public class ApplyServiceImpl implements IApplyService {
 
     @Override
     public ResultVO<String> updateApplyInfo(ApplyInfoDTO applyInfoDTO) {
-        if(!GenericUtils.ofNullable(applyInfoDTO) || !GenericUtils.ofNullable(applyInfoDTO.getOpenid()) || applyInfoDTO.getStatus() != 0){
+        if ( !GenericUtils.ofNullable(applyInfoDTO) || !GenericUtils.ofNullable(applyInfoDTO.getOpenid()) || applyInfoDTO.getStatus() != 0 ) {
             return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION,"参数有误",null);
+        }
+        // 判断是否已经到了开始面试的进度
+        if ( !Objects.equals(stringRedisTemplate.opsForValue().get(RedisKeyConstant.PROCESS), Integer.toString(RecruitStatusConstant.APPLY))) {
+            return new ResultVO<>(ResultStatusCodeConstant.FORBIDDEN,"不在报名阶段",null);
         }
         UpdateWrapper<ApplyInfoDTO> wrapper = new UpdateWrapper<>();
         wrapper.eq("openid",applyInfoDTO.getOpenid());
         int update = applyMapper.update(applyInfoDTO, wrapper);
-        if(update == 1){
+        if ( update == 1 ) {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"更新成功",applyInfoDTO.getName());
-        }else{
+        } else {
             return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND,"更新失败，请检查您的信息",null);
         }
     }
 
     @Override
     public ResultVO<Integer> signIn(String openid) throws NumberFormatException {
-        if(!GenericUtils.ofNullable(openid)){
+        if ( !GenericUtils.ofNullable(openid) ) {
             return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION,"参数有误",null);
         }
         UpdateWrapper<ApplyInfoDTO> wrapper = new UpdateWrapper<>();
@@ -100,10 +109,10 @@ public class ApplyServiceImpl implements IApplyService {
         ApplyInfoDTO applyInfoDTO = new ApplyInfoDTO();
         applyInfoDTO.setStatus(1);
         int update = applyMapper.update(applyInfoDTO, wrapper);
-        if(update == 1){
+        if ( update == 1 ) {
             String process = stringRedisTemplate.opsForValue().get(RedisKeyConstant.PROCESS);
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"签到成功",Integer.parseInt(process));
-        }else{
+        } else {
             return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND,"签到失败，请检查您的状态",null);
         }
     }
