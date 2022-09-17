bd201a741d01d728b08a1a4ebc6d8a41f18a1570
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-09-17 16:54:56 +0800

    fix( GlobalExceptionHandler ):返回异常同时设置响应代码
    返回异常同时设置响应代码

--- cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/utils/GlobalExceptionHandler.java
+++ cloud-consumer-recruit91/src/main/java/com/gdutelc/recruit/utils/GlobalExceptionHandler.java
@@ -10,6 +10,8 @@ import feign.RetryableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 全局异常处理器
  *
@@ -19,24 +21,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResultVO exceptionHandler(Exception e){
+    @ExceptionHandler(value = Exception.class)
+    public ResultVO<Void> exceptionHandler(HttpServletResponse response, Exception e){
         String msg = e.getMessage();
         if ( e instanceof LoginException ) {
-            return new ResultVO(ResultStatusCodeConstant.FORBIDDEN, msg);
+            response.setStatus(ResultStatusCodeConstant.FORBIDDEN);
+            return new ResultVO<>(ResultStatusCodeConstant.FORBIDDEN, msg);
         }
         if ( e instanceof ParamValidateException ) {
-            return new ResultVO(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, msg);
+            response.setStatus(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION);
+            return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION, msg);
         }
         if ( e instanceof BusinessException ) {
-            return new ResultVO(ResultStatusCodeConstant.BUSINESS_EXCEPTION, msg);
+            response.setStatus(ResultStatusCodeConstant.BUSINESS_EXCEPTION);
+            return new ResultVO<>(ResultStatusCodeConstant.BUSINESS_EXCEPTION, msg);
         }
         if ( e instanceof ServerException ) {
-            return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg);
+            response.setStatus(ResultStatusCodeConstant.SERVER_ERROR);
+            return new ResultVO<>(ResultStatusCodeConstant.SERVER_ERROR, msg);
         }
         if (e instanceof RetryableException) {
-            return new ResultVO(ResultStatusCodeConstant.SERVER_TIME_OUT, msg);
+            response.setStatus(ResultStatusCodeConstant.SERVER_TIME_OUT);
+            return new ResultVO<>(ResultStatusCodeConstant.SERVER_TIME_OUT, msg);
         }
-        return new ResultVO(ResultStatusCodeConstant.SERVER_ERROR, msg);
+        response.setStatus(ResultStatusCodeConstant.SERVER_ERROR);
+        return new ResultVO<>(ResultStatusCodeConstant.SERVER_ERROR, msg);
     }
 }
