d7611335dde35aeea33f40dd3fe645496ffb3c2a
cherry_jerry <cherryjerry626@gmail.com>
2022-10-03 16:24:44 +0800

    feat( ContentManageService ):新增消息内容管理服务接口

--- cloud-provider-message9003/src/main/java/com/gdutelc/recruit/service/interfaces/ContentManageService.java
+++ cloud-provider-message9003/src/main/java/com/gdutelc/recruit/service/interfaces/ContentManageService.java
@@ -2,12 +2,21 @@ package com.gdutelc.recruit.service.interfaces;
 
 import com.gdutelc.recruit.enums.Usage;
 
+import java.util.Map;
+
 /**
  * 集中管理消息推送的内容
  * @author cherry_jerry
  * @date 2022/10/03 13:12
  */
 public interface ContentManageService {
+
+    /**
+     * 获取完整消息模型
+     * @param usage 消息推送的使用场景
+     * @return 消息模型，包括模板ID、字段字符串数组和内容字符串数组
+     */
+    Map<String,Object> getPackedModel(Usage usage);
 
     /**
      * 获取模板id
