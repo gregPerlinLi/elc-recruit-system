e89d11c60ca5a3a20eb77eef1ac13673c7dca882
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-10-04 23:49:08 +0800

    fix( GetNameList ):添加注释
    添加注释

--- cloud-provider-message9003/src/main/java/com/gdutelc/recruit/service/interfaces/IGetNameList.java
+++ cloud-provider-message9003/src/main/java/com/gdutelc/recruit/service/interfaces/IGetNameList.java
@@ -10,28 +10,32 @@ public interface IGetNameList {
 
     /**
      * 获取报名名单
-     * @return
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
      */
     ResultVO<byte[]> getApplyList(Integer deptId);
 
     /**
      * 获取第一次面试通过名单
-     * @param deptId
-     * @return
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
      */
     ResultVO<byte[]> getFirstPassList(Integer deptId);
 
     /**
      * 获取第二次面试通过名单
-     * @param deptId
-     * @return
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
      */
     ResultVO<byte[]> getSecondPassList(Integer deptId);
 
     /**
      * 获取第二次面试调剂名单
-     * @param deptId
-     * @return
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
      */
     ResultVO<byte[]> getSecondAdjustList(Integer deptId);
 }
