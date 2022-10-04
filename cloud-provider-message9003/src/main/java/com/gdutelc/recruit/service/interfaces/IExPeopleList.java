700aca85aeba11a343acf580e53df44368ffe981
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-10-04 23:48:51 +0800

    fix( ExPeopleList ):添加注释
    添加注释

--- cloud-provider-message9003/src/main/java/com/gdutelc/recruit/service/interfaces/IExPeopleList.java
+++ cloud-provider-message9003/src/main/java/com/gdutelc/recruit/service/interfaces/IExPeopleList.java
@@ -10,27 +10,36 @@ public interface IExPeopleList {
 
     /**
      * 导出报名名单
-     * @return
+     *
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     ResultVO exportApplyList();
 
     /**
      * 导出一面通过名单
-     * @return
+     *
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     ResultVO exportFirstPassList();
 
     /**
      * 导出二面通过名单
-     * @return
+     *
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     ResultVO exportSecondPassList();
 
     /**
      * 导出二面调剂通过名单
-     * @return
+     *
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
      */
     ResultVO exportSecondAdjustPassList();
 
+    /**
+     * 导出最终录取名单
+     *
+     * @return {@link ResultVO}，其中不包含数据，只包含状态码和信息
+     */
     ResultVO exportAdmissionList();
 }
