85c771a870e6c08c487104770379dc9cca56abb5
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-10-04 23:48:26 +0800

    fix( ExportController ):添加注释
    添加注释

--- cloud-provider-message9003/src/main/java/com/gdutelc/recruit/controller/ExportController.java
+++ cloud-provider-message9003/src/main/java/com/gdutelc/recruit/controller/ExportController.java
@@ -1,29 +1,17 @@
 package com.gdutelc.recruit.controller;
 
 import com.gdutelc.recruit.constant.ResultStatusCodeConstant;
-import com.gdutelc.recruit.domain.entities.StuInfo;
 import com.gdutelc.recruit.domain.vo.ResultVO;
-import com.gdutelc.recruit.service.interfaces.IExPeopleList;
 import com.gdutelc.recruit.service.interfaces.IGetNameList;
-import com.gdutelc.recruit.service.interfaces.IPassListService;
-import com.gdutelc.recruit.utils.GenericUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.List;
 
 /**
  * @author TUFSolareyes
  * @date 22/10/02
  */
-
 @RestController
 @RequestMapping(value = "/pro/super_admin/elc_access")
 public class ExportController {
@@ -31,6 +19,13 @@ public class ExportController {
     @Resource
     private IGetNameList iGetNameList;
 
+    /**
+     * 获取报名名单
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
+     * @throws UnsupportedEncodingException 不支持的编码异常
+     */
     @GetMapping(value = "/getApply/{deptId}")
     public ResultVO<byte[]> getApplyNameList(@PathVariable("deptId") Integer deptId) throws UnsupportedEncodingException {
         ResultVO<byte[]> applyList = iGetNameList.getApplyList(deptId);
@@ -38,6 +33,12 @@ public class ExportController {
         return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"传输成功",data);
     }
 
+    /**
+     * 获取第一次面试通过名单
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
+     */
     @GetMapping(value = "/getFirst/{deptId}")
     @ResponseBody
     public ResultVO<byte[]> getFirstList(@PathVariable("deptId") Integer deptId) {
@@ -47,6 +48,12 @@ public class ExportController {
         return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"传输成功",data);
     }
 
+    /**
+     * 获取第二次面试通过名单
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
+     */
     @GetMapping(value = "/getSecond/{deptId}")
     @ResponseBody
     public ResultVO<byte[]> getSecondList(@PathVariable("deptId") Integer deptId) {
@@ -55,6 +62,12 @@ public class ExportController {
         return new ResultVO<>(ResultStatusCodeConstant.SUCCESS,"传输成功",data);
     }
 
+    /**
+     * 获取第二次面试调剂名单
+     *
+     * @param deptId 部门ID
+     * @return {@link ResultVO}，其中数据为字节流
+     */
     @GetMapping(value = "/getSecondAdjust/{deptId}")
     @ResponseBody
     public ResultVO<byte[]> getSecondAdjustList(@PathVariable("deptId") Integer deptId) {
